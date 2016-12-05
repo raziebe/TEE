537131b326e6d020705717d9fe4b0b227075e8ce
raz <raz@infinityar.com>
2016-12-06 01:32:42 +0200

    compilation

--- compile.sh
+++ compile.sh
@@ -1,2 +1,6 @@
-make CROSS_COMPILE=/opt/truly/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- ARCH=arm64 -j3 LOCALVERSION=-razkernel
-#make CROSS_COMPILE=/opt/truly/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- ARCH=arm64 -j3  deb-pkg LOCALVERSION=-razkernel
+# make -j4 CROSS_COMPILE=/opt/truly/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- ARCH=arm64  O=/opt/truly/OBJ-truly/ Image.gz
+
+make -j4 CROSS_COMPILE=/opt/truly/gcc-linaro-4.9-2015.02-3-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- ARCH=arm64  O=/opt/truly/OBJ-truly/ Image.gz
+
+scp ../OBJ-truly/arch/arm64/boot/Image.gz linaro@192.168.1.17:/boot/vmlinuz-4.4.11-trly+
+ssh  linaro@192.168.1.17
