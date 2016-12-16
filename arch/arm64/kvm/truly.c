5471bd043d83d056b1a0a2b5dbdcb11ac3a1f7a9
raz <raz@infinityar.com>
2016-12-16 21:02:33 +0200

    crash

--- arch/arm64/kvm/truly.c
+++ arch/arm64/kvm/truly.c
@@ -1,17 +1,5 @@
 #include <asm/sections.h>
-
-#include <linux/errno.h>
-#include <linux/err.h>
-#include <linux/kvm_host.h>
 #include <linux/module.h>
-#include <linux/vmalloc.h>
-#include <linux/fs.h>
-#include <asm/cputype.h>
-#include <asm/uaccess.h>
-#include <asm/kvm.h>
-#include <asm/kvm_asm.h>
-#include <asm/kvm_emulate.h>
-#include <asm/kvm_coproc.h>
 
 // .pushsection    .hyp.text, "ax"
 asm(".pushsection .hyp.text, \"ax\" \n\t" );
