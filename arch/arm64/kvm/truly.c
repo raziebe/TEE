e1f087f056c09ca9913aef9fbe7fb8dd139480d7
raz <raz@infinityar.com>
2016-12-17 23:26:00 +0200

    crash

--- arch/arm64/kvm/truly.c
+++ arch/arm64/kvm/truly.c
@@ -4,10 +4,30 @@
 
 asm(	".align 11;\n");
 
+int x;
+
+static inline int foo2(void)
+{
+	return 222;
+}
+
+static inline int foo3(void)
+{
+	return 333;
+}
+
+static inline int foo1(void)
+{
+	if (x == 1)
+		return foo2();
+	return foo3();
+
+}
+
 int truly_enter(void) 
 {
-	printk("XXXXXXXXXXX\n");
-	return 0x888;
+	x = 2;
+	return foo1();
 } 
 
 int truly_enter(void) __attribute__ (( section (".hyp.text") ));
