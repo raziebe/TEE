b1c774270ffec48dd2b9a63bcbf1ca360cb0ffe8
raz <raz@infinityar.com>
2017-02-15 22:17:43 +0000

    same as before #5 entry

--- arch/arm64/kvm/truly/truly.c
+++ arch/arm64/kvm/truly/truly.c
@@ -282,9 +282,9 @@ int truly_test_vttbr(void *cxt)
 	make_vtcr_el2();
 	make_sctlr_el2();	
 
-	truly_set_vttbr_el2(tvm.vttbr_el2);
-	truly_set_vtcr_el2(tvm.vtcr_el2);
-	truly_set_sctlr_el2(tvm.sctlr_el2);
+//	truly_set_vttbr_el2(tvm.vttbr_el2);
+//	truly_set_vtcr_el2(tvm.vtcr_el2);
+//	truly_set_sctlr_el2(tvm.sctlr_el2);
 	// page 5295
 	// should enable DC or VM to have a second stage 
 	// translationa
